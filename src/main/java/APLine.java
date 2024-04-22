f22b57c04bb7f7d29a246d541646624a1f9bcc6d
koa-s <143123833+koa-s@users.noreply.github.com>
2024-04-22 11:17:49 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,22 @@
 public class APLine
 {
-  /* your code here */
-  
+  private int a, b, c;
+  private double slope;
+  APLine(int 1, int 2, int 3)
+  {
+    a=1;
+    b=2;
+    c=3;
+    slope = -((double)(a))/b;
+  }
+  public int getSlope()
+  {
+    return slope;
+  }
+  public boolean isOnLine(x,y)
+  {
+    if(a*x+b*y+c==0)
+      return true;
+    return false;
+  }
 }
